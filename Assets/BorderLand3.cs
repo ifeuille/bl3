1034369c96d85799db78f0641818af35b4b14a88
noleafnolife <770829892@qq.com>
2020-03-14 13:09:05 +0800

    1

---
+++ Assets/BorderLand3.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class BorderLand3 : MonoBehaviour
+{
+    public new Camera camera;
+    public ComputeShader outLineComputeShader;
+    public Shader uberShader;
+    public Material material;
+    private RenderTexture m_OutlineTexture;
+
+    public struct CmdBufferEntry
+    {
+        public CommandBuffer m_AfterScene;
+    }
+    private Dictionary<Camera, CmdBufferEntry> m_Cameras = new Dictionary<Camera, CmdBufferEntry>();
+
+    private void Awake()
+    {
+        camera.depthTextureMode = DepthTextureMode.Depth;
+
+        m_OutlineTexture = new RenderTexture(Screen.width, Screen.height, 0, RenderTextureFormat.RG16);
+        m_OutlineTexture.name = "Outline RT";
+        m_OutlineTexture.enableRandomWrite = true;
+        m_OutlineTexture.Create();
+    }
+    private void OnDestroy()
+    {
+        m_OutlineTexture.Release();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //outLineComputeShader.SetTexture("_DepthTexture",)
+    }
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {        
+        Graphics.Blit(source, destination,material);
+    }
+    private void Draw()
+    {
+        var cam = Camera.main;
+        if (!cam) return;
+        CmdBufferEntry buf = new CmdBufferEntry();
+        if (m_Cameras.ContainsKey(cam))
+        {
+            // use existing command buffers: clear them
+            buf = m_Cameras[cam];
+            buf.m_AfterScene.Clear();
+        }
+        else
+        {
+            buf.m_AfterScene = new CommandBuffer();
+            buf.m_AfterScene.name = "DrawOutline";
+            m_Cameras[cam] = buf;
+            cam.AddCommandBuffer(CameraEvent.AfterForwardOpaque, buf.m_AfterScene);
+        }
+        var cb = buf.m_AfterScene;
+        cb.Clear();
+        cb.SetGlobalTexture("_OutlineTexture", m_OutlineTexture);
+        cb.SetGlobalTexture("_DepthTexture", cam.activeTexture.depthBuffer);
+        int x = (int)(Screen.width / 10 + 0.5);
+        int y = (int)(Screen.height / 10 + 0.5);
+
+        cb.DispatchCompute(outLineComputeShader, outLineComputeShader.FindKernel("CSMain"), x,y,1);
+        cb.SetRenderTarget(cam.activeTexture.colorBuffer, cam.activeTexture.depthBuffer);
+        
+        cb.DrawProcedural(Matrix4x4.identity, material, 0, MeshTopology.Triangles, 3);
+
+
+    }
+
+}
---
+++ Assets/Uber.shader
@@ -0,0 +1,90 @@
+﻿Shader "Unlit/Uber"
+{
+    Properties
+    {
+        //_MainTex ("Texture", 2D) = "white" {}
+		_OutlineColor ("OutlineColor",Color) = (1,1,1,1)
+		_FilterValue("FilterValue",Range(0,1))=0.374
+    }
+    SubShader
+    {
+        Tags { "RenderType"="Opaque" }
+        LOD 100
+		Cull Off ZWrite On ZTest Off
+		// Blending matches SDK Sample, which 
+		// works in terms of (1-a) and otherwise premultiplied.
+		Blend One SrcAlpha
+		ColorMask RGBA
+
+		/*Stencil {
+			Ref 0
+			Comp Less
+			ReadMask 255
+			WriteMask 0
+			Pass Keep
+			Fail Keep
+			ZFail Keep
+		}*/
+
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+            // make fog work
+            #pragma multi_compile_fog
+
+            #include "UnityCG.cginc"
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+				uint vid : SV_VertexID;
+            };
+
+			struct v2f {
+				float4 pos : SV_POSITION;
+				float4 scrPos:TEXCOORD1;
+			};
+
+            //sampler2D _MainTex;
+            //float4 _MainTex_ST;
+			sampler2D _OutlineTexture;
+			float4 _OutlineTexture_ST;
+			sampler2D _CameraDepthTexture;
+
+			float _FilterValue;
+			half4 _OutlineColor;
+			half4 vertexs[3] = {
+				{-1,0,0,1},
+				{1,0,0,1},
+				{0,1,0,1}
+			};
+			//Vertex Shader  
+			v2f vert (appdata v) {
+
+				v2f o;
+				o.pos = vertexs[v.vid];// UnityObjectToClipPos (v.vertex);
+				o.scrPos = ComputeScreenPos (o.pos);
+				o.scrPos = vertexs[v.vid];
+				return o;
+			}
+			//Fragment Shader  
+			half4 frag (v2f i) : COLOR{
+				return half4(1,0,0,1);
+			    float depthValue = Linear01Depth (tex2Dproj (_CameraDepthTexture,UNITY_PROJ_COORD (i.scrPos)).r);
+				bool needClip = (0.00001 >= depthValue) ? true : false;
+				if (needClip)discard;
+				float outline = tex2D (_OutlineTexture,i.scrPos).r;
+				if (outline < _FilterValue)
+				{
+					return _OutlineColor;
+				}
+				return float4(0, 0, 0, 0);
+				//float4 color = tex2D (_MainTex, i.scrPos);
+			}
+            ENDCG
+        }
+    }
+}
